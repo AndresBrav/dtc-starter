5e1c62b3ad3227c513944507b2820e6a43e222dd
Andres Bravo <bravoandres1706@gmail.com>
2026-05-14 06:02:57 -0400

    feat: prueba con mi nombre

--- apps/storefront/src/modules/home/components/hero/index.tsx
+++ apps/storefront/src/modules/home/components/hero/index.tsx
@@ -9,13 +9,13 @@ const Hero = () => {
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Ecommerce Starter Template
+            Ecommerce Starter Template Andres
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Powered by Medusa and Next.js
+            Powered by Medusa and Next.js we can edit all the content in the dashboard
           </Heading>
         </span>
         <a href="https://github.com/medusajs/dtc-starter" target="_blank">
